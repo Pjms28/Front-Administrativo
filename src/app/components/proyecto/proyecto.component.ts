e9a7dfab1fcd584a8091b4320d0a3f011d0c64c7
Josias Velazquez <jossias.vel@gmail.com>
2019-06-23 19:06:04 -0400

    conflictos

--- src/app/components/proyecto/proyecto.component.ts
+++ src/app/components/proyecto/proyecto.component.ts
@@ -1,10 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-<<<<<<< HEAD
-import { ImagenProyecto } from 'imagen-proyecto.model';
 import { ImagenesModel } from 'src/app/modelos/imagenes.model';
-=======
-import { ImagenesModel } from 'src/app/modelos/imagenes.model';
->>>>>>> 1536f9c6ccfae7b5023b2460814189b3bcf840c8
 
 
 @Component({
