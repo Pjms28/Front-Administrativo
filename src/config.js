0329da387e4021ba00a1f470e372c70286a74a47
Josias Velazquez <jossias.vel@gmail.com>
2019-07-16 20:47:26 -0400

    jossias

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 function env(env) {
     switch (env) {
         case 'api-netcore':
-            return 'https://api-constructoramp.azurewebsites.net/api';
+            return 'http://api-brick.azurewebsites.net/api';
         case 'front-admi':
             return 'http://constructoramejiapolanco.azurewebsites.net/login';
     }
